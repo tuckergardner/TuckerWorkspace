5bc9b3d68f4c14863f56e4bbda667b2b89186b76
ceb-student <CEB-Student@caddoschools.org>
2017-10-20 09:01:51 -0500

    msade math trunner

---
+++ GCF Algarisms/src/gcfalgorism/EuclidGCF.java
@@ -0,0 +1,5 @@
+package gcfalgorism;
+
+public class EuclidGCF {
+
+}
---
+++ GCF Algarisms/src/gcfalgorism/MathRunner.java
@@ -0,0 +1,11 @@
+package gcfalgorism;
+
+public class MathRunner {
+
+	public static void main(String[] args)
+	{
+		
+
+	}
+
+}
